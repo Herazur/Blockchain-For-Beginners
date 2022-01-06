444e0929751b7cd39c6f08cd0380c675efddfd3c
Furkan <68228757+Herazur@users.noreply.github.com>
2022-01-06 18:02:06 +0300

    Update Blockchain-For-Beginners#3.sol

--- Blockchain For Beginners #3/Blockchain-For-Beginners#3.sol
+++ Blockchain For Beginners #3/Blockchain-For-Beginners#3.sol
@@ -1,6 +1,18 @@
 pragma solidity 0.8.10;
 
 contract MyContract {
-    uint public x = 21;
+    address public lastSender;
 
+    function recieve() external payable {
+        lastSender = msg.sender;
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function pay (address payable addr) public payable {
+        (bool sent, bytes memory data) = addr.call{value: 1 ether}("");
+        require(sent, "Error sending eth.");
+    }
 }
